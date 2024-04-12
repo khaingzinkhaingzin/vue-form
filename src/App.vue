3b0897288342599b7dab5bb6562e92563a31df97
khaingzinkhaingzin <khaingthazin8july@gmail.com>
2024-04-12 21:37:14 +0630

    v-model two way binding, keyboard event

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <SignUp />
+</template>
+
+<script>
+import SignUp from "./components/SignUp.vue"
+export default {
+  components: {
+    SignUp,
+  }
+}
+</script>
+
+<style>
+    body {
+        background: rgb(241, 250, 255);
+    }
+    form {
+        max-width: 420px;
+        margin: 30px auto;
+        background: white;
+        text-align: left;
+        padding: 40px;
+        border-radius: 10px;
+    }
+    label {
+        color: #aaa;
+        display: inline-block;
+        margin: 25px 0 15px;
+        font-size: 0.6em;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        font-weight: bold;
+    }
+    input,
+    select {
+        display: block;
+        padding: 10px 6px;
+        width: 100%;
+        box-sizing: border-box;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        color: #555;
+    }
+    input[type="checkbox"] {
+        display: inline-block;
+        width: 16px;
+        margin: 0 10px 0 0;
+        position: relative;
+        top: 2px;
+    }
+</style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,59 @@
+<template>
+  <form>
+    <label for="">Email</label>
+    <input type="email" required v-model="email">
+
+    <label for="">Password</label>
+    <input type="password" required v-model="password">
+
+    <label for="">Role</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <div>
+      <label for="">Skills</label>
+      <input type="text" @keyup="addSkill" v-model="skill">
+    </div>
+
+    <p>{{ skills }}</p>
+
+    <div>
+      <input type="checkbox" v-model="accept">
+      <label for="">Accept Terms and condition</label>
+    </div>
+    
+  </form>
+  <p>email - {{ email }}</p>
+  <p>password - {{ password }}</p>
+  <p>role - {{ role }}</p>
+  <p>accept - {{ accept }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      accept: false,
+      skills: [],
+      skill: "",
+    }
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === ",") {
+        this.skills.push(this.skill);
+        this.skill = "";
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
